7fb484ed56e681f5e8de6690cbf4b0251123d4e1
Wanderson Oliveira <wanderson1873@live.com>
2020-12-17 16:22:41 +0000

    MVC

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,7 @@
+// index, show, store, update, destrov
+
+module.exports = {
+    store(req, res) {
+        return res.json({ message: 'Hello World' })
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,8 @@
-const { Router } = require('express')
 const express = require('express')
+const SessionController = require('./controllers/SessionController')
 
 const routes = express.Router()
 
-routes.post('/users', (req, res) => {
-    return res.json(req.body)
-})
+routes.post('/users', SessionController.store})
 
 module.exports = routes
