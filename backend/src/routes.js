d99794e448d516129a5a05fdaacaf19258df20f8
Wanderson Oliveira <wanderson1873@live.com>
2020-12-17 16:09:40 +0000

    bd

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,10 @@
+const { Router } = require('express')
 const express = require('express')
 
 const routes = express.Router()
 
-app.get('/', (req, res) => {
-    return res.json( {mensage: "Hello World"} )
+routes.post('/users', (req, res) => {
+    return res.json(req.body)
 })
+
+module.exports = routes
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const routes = require('./routes')
 const app = express()
+
+mongoose.connect('mongodb+srv://wanderson1873:86927233@aircnc.wiibl.mongodb.net/aircnc?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 // GET, POST, PUT, DELETE
 
@@ -10,6 +17,5 @@ const app = express()
 
 app.use(express.json())
 app.use(routes)
-
 
 app.listen(3333)
