ac9d9ee9e7f12c7a6267fbea9d50e3b44bfe537f
Wanderson Oliveira <wanderson1873@live.com>
2020-12-18 18:02:27 +0000

    Update SessionController.js

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -5,7 +5,7 @@ module.exports = {
     async store(req, res) {
         const { email } = req.body
 
-        const user = await User.create({ email })
+        const user = await User.create({email})
 
         return res.json(user)
     }
