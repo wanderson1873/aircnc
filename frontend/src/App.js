4790e19925e2cc01cd558a4670eda37371ff5f16
Wanderson Oliveira <wanderson1873@live.com>
2020-12-24 13:18:35 +0000

    add form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className="container">
+        <img src={logo} alt="logo AirCnc"/>
+
+        <div className="content">
+            <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
+
+            <form action="">
+              <label htmlFor="email">E-MAIL *</label>
+              <input 
+                id="email"
+                type="email"
+                placeholder="Seu melhor e-mail"
+              />
+
+              <button className="btn" type="submit">Entrar</button>
+            </form>
+        </div>
+    </div>
   );
 }
 
