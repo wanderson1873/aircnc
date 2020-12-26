9ff527fb4d9ae66ee1ef453a49c6dc9a52329d2b
Wanderson Oliveira <wanderson1873@live.com>
2020-12-26 18:09:53 +0000

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api'
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log( email )
+
+  }
+
   return (
     <div className="container">
         <img src={logo} alt="logo AirCnc"/>
@@ -11,12 +21,14 @@ function App() {
         <div className="content">
             <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
 
-            <form action="">
+            <form onSubmit={handleSubmit}>
               <label htmlFor="email">E-MAIL *</label>
               <input 
                 id="email"
                 type="email"
                 placeholder="Seu melhor e-mail"
+                value={email}
+                onChange={event =>setEmail(event.target.value)}
               />
 
               <button className="btn" type="submit">Entrar</button>
